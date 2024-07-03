5282b69961e07d161d8e76d99e94093596866457
Billy Iea <95914478+Numyu@users.noreply.github.com>
2024-07-03 10:49:27 +0200

    Connect mqtt (#7)
    * add service mqtt
    
    * refactor mqtt service + update schema prisma
    
    * MQTT service correction to be able to subscribe to several subjects
    
    * add topic
    
    * add mqtt dependencies
    
    ---------
    
    Co-authored-by: mat <112476420+matteoLED@users.noreply.github.com>

---
+++ controllers/ActionsController.ts
@@ -0,0 +1,8 @@
+
+
+
+async function eventAction(topic: string, message: string) {
+    console.log('eventAction', topic, message);
+}
+
+export default { eventAction };
---
+++ service/mqtt.ts
@@ -0,0 +1,62 @@
+import mqtt from 'mqtt';
+import ActionsController from '../controllers/ActionsController';
+
+class MqttService {
+  private client: mqtt.MqttClient;
+
+  constructor() {
+    this.client = mqtt.connect('mqtt://192.168.204.168:1883');
+  }
+
+  // Fonction pour se connecter au broker MQTT
+  public connect() {
+    this.client.on('connect', () => {
+      console.log('MQTT connected');
+      this.subscribeToTopic(['esp32/track', 'esp32/sonar', 'esp32/light']);
+    });
+  }
+
+  // Fonction pour s'abonner à un topic spécifique
+  private subscribeToTopic(topics: string[]) {
+    this.client.subscribe(topics, (err) => {
+      if (err) {
+        console.error(`Failed to subscribe to topic ${topics}:`, err);
+      } else {
+        console.log(`Subscribed to topic ${topics}`);
+      }
+    });
+
+    this.client.on('message', (receivedTopic, message) => {
+      if (topics.includes(receivedTopic)) {
+        this.handleMessage(receivedTopic, message.toString());
+      }
+    });
+  }
+
+  // Fonction pour publier un message à un topic spécifique
+  public publish(topic: string, message: string) {
+    this.client.publish(topic, message, (err) => {
+      if (err) {
+        console.error(`Failed to publish message to topic ${topic}:`, err);
+      } else {
+        console.log(`Message published to topic ${topic}`);
+        this.handleMessage(topic, message);
+      }
+    });
+  }
+
+  // Fonction pour gérer les messages et appeler l'ActionsController
+  private handleMessage(topic: string, message: string) {
+    ActionsController.eventAction(topic, message);
+  }
+
+  // Fonction pour fermer la connexion MQTT
+  public endConnection() {
+    this.client.end(() => {
+      console.log('MQTT connection closed');
+    });
+  }
+}
+
+const mqttService = new MqttService();
+export default mqttService;
