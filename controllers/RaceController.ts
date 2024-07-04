ac8111880a42589bfa17c4da3dc7a71f6e96ec3b
Billy IEA <iea.billy.kd@gmail.com>
2024-07-04 15:11:14 +0200

    refactor: remove unused function

--- controllers/RaceController.ts
+++ controllers/RaceController.ts
@@ -19,33 +19,6 @@ const getRaces = async (_: Request, res: Response): Promise<void> => {
   }
 };
 
-const createRace = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { timestamp, startTime, endTime, distance } = req.body;
-
-    const raceData = {
-      timestamp: new Date(timestamp),
-      start_Time: new Date(startTime),
-      end_Time: new Date(endTime),
-      distance: parseFloat(distance),
-    };
-
-    const newRace = await RaceRepository.createRace(raceData);
-
-    res.status(201).json({
-      status: 'success',
-      data: newRace,
-    });
-  } catch (error: any) {
-    if (error instanceof Error) {
-      res.status(400).json({ error: error.message });
-    } else {
-      res.status(400).json({ error: 'An unknown error occurred' });
-    }
-  }
-};
-
 export default {
   getRaces,
-  createRace,
 };
--- router/race.route.ts
+++ router/race.route.ts
@@ -7,7 +7,4 @@ const route = Router();
 // Method: GET
 route.get('/all', RaceController.getRaces);
 
-// Method: POST
-route.post('/create', RaceController.createRace);
-
 export default route;
