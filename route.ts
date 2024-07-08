317b6dfac52c806c4e7241bd62f7e420e93ff902
Billy IEA <iea.billy.kd@gmail.com>
2024-07-08 13:11:48 +0200

    feat: api key middleware

--- route.ts
+++ route.ts
@@ -3,9 +3,11 @@ import { Express, Router } from 'express';
 import raceRoute from './router/race.route';
 import speedRoute from './router/speed.route';
 import orientationRoute from './router/orientation.route';
+import { apiKeyMiddleware } from './middleware/securityMiddleware';
 
 const apiRouter = Router();
 
+apiRouter.use(apiKeyMiddleware);
 apiRouter.use('/race', raceRoute);
 apiRouter.use('/speed', speedRoute);
 apiRouter.use('/orientation', orientationRoute);
