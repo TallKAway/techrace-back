c9b167d15e2a302a38aca3c2e86392cb8921d886
Billy IEA <iea.billy.kd@gmail.com>
2024-07-08 15:40:41 +0200

    doc: orientation route + tag

--- config/swaggerConfig.ts
+++ config/swaggerConfig.ts
@@ -8,6 +8,11 @@ const options = {
       version: '1.0.0',
       description: 'Documentation for the Techrace API',
     },
+    tags: [
+      { name: 'Orientation', description: 'Operations related to orientation records' },
+      { name: 'Race', description: 'Operations related to races' },
+      { name: 'Speed', description: 'Operations related to speed records' },
+    ],
   },
   apis: ['./docs/swagger/*.ts'],
 };
