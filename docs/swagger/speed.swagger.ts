4810aa64772c6e4f0c36687d90a89b5e4222da16
Billy IEA <iea.billy.kd@gmail.com>
2024-07-12 20:47:00 +0200

    docs: change doc for speed

--- docs/swagger/speed.swagger.ts
+++ docs/swagger/speed.swagger.ts
@@ -53,7 +53,7 @@
 
 /**
  * @swagger
- * /api/speed/all/{id}:
+ * /api/speed/{id}:
  *   get:
  *     tags:
  *       - Speed
@@ -84,39 +84,46 @@
  *                   type: string
  *                   example: success
  *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: integer
- *                         example: 3
- *                       speed:
- *                         type: integer
- *                         example: 10
- *                       max_Speed:
- *                         type: integer
- *                         example: 20
- *                       average_Speed:
- *                         type: integer
- *                         example: 15
- *                       timestamp:
- *                         type: string
- *                         format: date-time
- *                         example: 2023-07-01T11:00:00.000Z
- *                       raceId:
- *                         type: integer
- *                         example: 3
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 2
+ *                     speeds:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           date:
+ *                             type: string
+ *                             format: date-time
+ *                             example: 2024-07-11T18:00:00
+ *                           speed:
+ *                             type: integer
+ *                             example: 18
+ *                     max_Speed:
+ *                       type: integer
+ *                       example: 20
+ *                     average_Speed:
+ *                       type: integer
+ *                       example: 18
+ *                     timestamp:
+ *                       type: string
+ *                       format: date-time
+ *                       example: 2024-01-11T14:59:48.000Z
+ *                     raceId:
+ *                       type: integer
+ *                       example: 2
  */
 
 /**
  * @swagger
- * /api/speed/all/race/{id}:
+ * /api/speed/race/{id}:
  *   get:
  *     tags:
  *       - Speed
- *     summary: Get all speed records for a specific race
- *     description: Returns a list of all speed records for a specific race by its ID
+ *     summary: Get speed record by race ID
+ *     description: Returns a single speed record by its ID
  *     produces:
  *       - application/json
  *     parameters:
@@ -127,12 +134,12 @@
  *         type: string
  *       - name: id
  *         in: path
- *         description: ID of the race to get the speed records for
+ *         description: ID of the speed record to return
  *         required: true
  *         type: integer
  *     responses:
  *       200:
- *         description: List of all speed records for the specified race
+ *         description: successful operation
  *         content:
  *           application/json:
  *             schema:
@@ -142,27 +149,34 @@
  *                   type: string
  *                   example: success
  *                 data:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: integer
- *                         example: 3
- *                       speed:
- *                         type: integer
- *                         example: 10
- *                       max_Speed:
- *                         type: integer
- *                         example: 20
- *                       average_Speed:
- *                         type: integer
- *                         example: 15
- *                       timestamp:
- *                         type: string
- *                         format: date-time
- *                         example: 2023-07-01T11:00:00.000Z
- *                       raceId:
- *                         type: integer
- *                         example: 3
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 2
+ *                     speeds:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           date:
+ *                             type: string
+ *                             format: date-time
+ *                             example: 2024-07-11T18:00:00
+ *                           speed:
+ *                             type: integer
+ *                             example: 18
+ *                     max_Speed:
+ *                       type: integer
+ *                       example: 20
+ *                     average_Speed:
+ *                       type: integer
+ *                       example: 18
+ *                     timestamp:
+ *                       type: string
+ *                       format: date-time
+ *                       example: 2024-01-11T14:59:48.000Z
+ *                     raceId:
+ *                       type: integer
+ *                       example: 2
  */
